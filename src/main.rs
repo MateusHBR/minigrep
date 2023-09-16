7c7991afd4c5cbc1581194b81abe7ad43768f04d
MateusHBR <mateusmorais.r@gmail.com>
2023-09-16 10:28:21 -0300

    read local file from args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::{env, fs};
+
 fn main() {
-    println!("Hello, world!");
+    let args = env::args().collect::<Vec<String>>();
+    let (Some(query), Some(file_path)) = (args.get(1), args.get(2)) else {
+        eprintln!("Expected args <query> <file-path>");
+        std::process::exit(1);
+    };
+
+    let Ok(content) = fs::read_to_string(file_path) else {
+        eprintln!("Could not read file \"{}\"", file_path);
+        std::process::exit(1);
+    };
+
+    println!("Searching for \"{}\" on \"{}\" ...", query, file_path);
+
+    println!("Content: \n{}", content);
 }
